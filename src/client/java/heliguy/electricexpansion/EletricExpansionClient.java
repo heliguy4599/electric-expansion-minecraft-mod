dec116491a98c5d1cdf524f9da99e15a7877270d
Heliguy <heliguy4599@hotmail.com>
2025-08-28 16:38:14 -0400

    Initial Commit

---
+++ src/client/java/heliguy/electricexpansion/EletricExpansionClient.java
@@ -0,0 +1,10 @@
+package heliguy.electricexpansion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EletricExpansionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
